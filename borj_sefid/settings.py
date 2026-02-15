d872fa57faed746dd413162a102e805a32f2bea4
Seyed Ali Reza Hashemi Rad <A.HashemiRad@Acmgroup.ir>
2026-02-15 16:35:39 +0330

    initialize for moin gardeshgari

--- borj_sefid/settings.py
+++ borj_sefid/settings.py
@@ -84,13 +84,17 @@ WSGI_APPLICATION = 'borj_sefid.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'borj_sefid_db',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'trmm3b42',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'borj_sefid_db',
-        'USER': 'postgres',
-        'PASSWORD': 'trmm3b42',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
